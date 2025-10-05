5e71f33adb412941681831c27490b0ef1183e1d3
indra <indra1nkuss@gmail.com>
2025-10-05 22:53:00 +0700

    Sistem Monitoring Energi

---
+++ bin/dart_monitoring_energi.dart
@@ -0,0 +1,5 @@
+import 'package:dart_monitoring_energi/dart_monitoring_energi.dart' as dart_monitoring_energi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_monitoring_energi.calculate()}!');
+}
---
+++ test/dart_monitoring_energi_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_monitoring_energi/dart_monitoring_energi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
